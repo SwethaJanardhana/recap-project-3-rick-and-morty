d69f69bd098f8f2f2352b999cd54d32a113ac568
Paul Johann Flade <paul-johann@flade-privat.de>
2024-01-30 12:56:09 +0100

    add refactoring update after hints

--- components/NavButton/NavButton.js
+++ components/NavButton/NavButton.js
@@ -1,16 +1,12 @@
-export function NavigationButtons() {
-  const previousButton = document.createElement("button");
-  previousButton.classList.add("button", "button--prev");
-  previousButton.setAttribute("data-js", "button-prev");
-  previousButton.textContent = "previous";
-
-  const nextButton = document.createElement("button");
-  nextButton.classList.add("button", "button--next");
-  nextButton.setAttribute("data-js", "button-next");
-  nextButton.textContent = "next";
-  const buttons = {
-    previous: previousButton,
-    next: nextButton,
-  };
-  return buttons;
+export function NavigationButtons(prop) {
+  const button = document.createElement("button");
+  button.classList.add("button");
+  button.classList.add(`button--${prop.eventType}`);
+  button.addEventListener("click", prop.onClick);
+  if (prop.eventType === "prev") {
+    button.textContent = "previous";
+  } else if (prop.eventType === "next") {
+    button.textContent = "next";
+  }
+  return button;
 }
